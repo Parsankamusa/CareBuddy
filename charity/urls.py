5865d0fe6cbc083ec4d15afea7488c8236a0f3aa
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-07-21 13:27:07 +0300

    Update urls.py

--- charity/urls.py
+++ charity/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_view
+from .views import  manageTemplateView, DonationTemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('Form/', views.profile, name='Form'),
     path('login/', auth_view.LoginView.as_view(template_name='charity/login.html'), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name='charity/logout.html'), name="logout"),
+    path('manage-Donations/', manageTemplateView.as_view(), name='manage'),
+    path('make-Donation/', DonationTemplateView.as_view(), name='Donation'),
     ]
